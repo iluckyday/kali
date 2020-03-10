b8b4fd39d50f6c004b4a0a002632f67ba103683c
iluckyday <luckyday1990@mail.com>
2020-03-10 08:39:41 +0000

    test

--- build.sh
+++ build.sh
@@ -2,8 +2,20 @@
 set -e
 
 apt-get update
+
+wget http://http.kali.org/pool/main/k/kali-archive-keyring/kali-archive-keyring_2018.2_all.deb
+wget https://archive.kali.org/kali/pool/main/l/live-build/live-build_20190311_all.deb
+dpkg -i kali-archive-keyring_2018.2_all.deb
+dpkg -i live-build_20190311_all.deb
+
 apt-get install -y live-build
 
+cd /usr/share/debootstrap/scripts/
+echo "default_mirror http://http.kali.org/kali"; sed -e "s/debian-archive-keyring.gpg/kali-archive-keyring.gpg/g" sid > /tmp/kali
+mv /tmp/kali .
+ln -s kali kali-rolling
+
+cd ~
 git clone --depth=1 https://gitlab.com/kalilinux/build-scripts/live-build-config.git
 cd live-build-config
 ./build.sh --variant minimal --verbose
